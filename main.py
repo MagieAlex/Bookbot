b97f2e447a5a79ae6444816a15af9c086606b392
MagieAlex <alexander.zie@mail.de>
2024-08-18 14:29:36 +0200

    book report added

--- main.py
+++ main.py
@@ -3,8 +3,28 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     letter_count = get_letter_count(text)
+    letter_list = dict_to_list(letter_count)
+#    print(letter_count)
+#    print(letter_list)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(letter_count)
+
+    for item in letter_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+def sort_list(d):
+    return d["num"]
+
+def dict_to_list(dict):
+    list = []
+    for ch in dict:
+        list.append({"char": ch, "num": dict[ch]})
+    list.sort(reverse=True, key=sort_list)
+    return list
 
 def get_letter_count(text):
     lower_text = text.lower()
